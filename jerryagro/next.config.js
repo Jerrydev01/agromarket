951b5bcda27c117803fa39b347dece1463ba9554
Jerry Francis <36851589+Jerrydev01@users.noreply.github.com>
2022-05-13 19:10:25 +0100

    Update next.config.js

--- jerryagro/next.config.js
+++ jerryagro/next.config.js
@@ -3,8 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-// module.exports = {
-//   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
-// }
-
-// module.exports = nextConfig
+module.exports = nextConfig
