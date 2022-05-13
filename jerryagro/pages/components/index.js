ebde347d3bf43a50392afa75eb7d225e6ef5f27d
zeus012345 <francisjerry360@gmail.com>
2022-05-13 08:08:20 +0100

    master

--- jerryagro/pages/components/index.js
+++
@@ -1,6 +0,0 @@
-import { default as HeroBanner } from './HeroBanner'
-import { default as Navbar } from './Navbar'
-import { default as Footer } from './Footer'
-import { default as Product } from './Product'
-import { default as Cart } from './Cart'
-import { default as FooterBanner } from './FooterBanner'
